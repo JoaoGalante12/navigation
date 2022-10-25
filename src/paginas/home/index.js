88419c87971656e134bb248ee600d73ae05a62d3
vicLira <victor.liracarlos@gmail.com>
2022-10-25 10:28:46 -0300

    flatlist

--- src/paginas/home/index.js
+++ src/paginas/home/index.js
@@ -1,30 +1,38 @@
 import { useNavigation } from '@react-navigation/native';
 import React,{useState,useEffect} from 'react'
-import { StyleSheet, Text, View, TouchableOpacity,Button} from 'react-native';
+import { Children } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,Button,FlatList} from 'react-native';
 import firebase  from '../../firebaseConnection';
 
 
 
 export default function Home(){
 
-const [nome,setNome] = useState("");
+const [alunos,setNome] = useState([]);
 
-useEffect( () => {
+useEffect(() =>{
 
 
     async function buscarNome() {
-        await firebase.database().ref('Aluno/2/Nome').on('value',(snapshot)=> {
+
+        await firebase.database().ref('Aluno').on('value',(snapshot)=> {
             setNome(snapshot.val())
         });
-
+        snapshot.forEach((Children) =>{
+            var data = {
+                key: childItem.key,
+                nome: childItem.val().nome,
+                nota1: childItem.val().nota1,
+                nota2: childItem.val().nota2,
+                nota3: childItem.val().nota3,
+                imagem: childItem.val().imagem
+            };
+            setAlunos(alunos =>[...alunos,data])
     }
+        )
     
 buscarNome();
-
-
-
-},[]  )
-
+    }}, [])
 
 return(
 
